b83e429794ab6b7d2f9a2ffb6c713ea9b4964c95
Ziminator <aleksander.zimin@gmail.com>
2019-10-03 07:39:05 -0700

    Homework tests controller

--- Gemfile
+++ Gemfile
@@ -27,11 +27,12 @@ gem 'jbuilder', '~> 2.7'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'slim-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'rspec-rails', '~> 3.8'
+  gem 'rspec-rails'
   gem 'factory_bot_rails'
 end
 
---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    it 'populates an array of all questions' do
+      question1 = FactoryBot.create(:question)
+      question2 = FactoryBot.create(:question)
+      get :index
+      expect(assigns(:questions)).to match_array([question1, question2])
+    end
+    it 'renders index view' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+end
