7cb922c71672ce3fbde3e17f364569b835fcc610
ziminator <aleksander.zimin@gmail.com>
2019-10-04 07:26:22 +0300

    Homework 3 tests are finished

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,12 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @questions = Question.new
+  end
 end
