50c2fd93a26c50f364e62112d006f5c20cd0f6d1
ziminator <aleksander.zimin@gmail.com>
2019-10-31 13:33:10 +0300

    Lesson-6 fix homework version 3

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -37,9 +37,6 @@ feature 'User can create question', %q{
   scenario 'Unauthenticated user asks a question with errors' do
     visit questions_path
     expect(page).to_not have_button 'Log in'
-    #click_on 'Ask question'
-    #save_and_open_page
-    #expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
 
