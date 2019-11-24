c718b11d50382ce0d2d366780065c7ea906e60dc
ziminator <aleksander.zimin@gmail.com>
2019-11-25 00:04:48 +0300

    Lesson-8 homework

--- spec/features/answers/create_spec.rb
+++ spec/features/answers/create_spec.rb
@@ -28,6 +28,16 @@ feature 'User can create answer', %q{
       click_on 'Answer'
       expect(page).to have_content "Body can't be blank"
     end
+
+    scenario 'answer the question with attached file' do
+      fill_in 'Body', with: 'answer body'
+
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Answer'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
   end
 
   scenario 'Not authenticated user answer a question' do
