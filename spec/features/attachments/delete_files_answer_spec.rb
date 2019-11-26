5ee3d2847eeaa1385dc204afc9bf54109a5a9b95
ziminator <aleksander.zimin@gmail.com>
2019-11-27 01:30:50 +0300

    Lesson-8 finish homework

--- spec/features/attachments/delete_files_answer_spec.rb
+++ spec/features/attachments/delete_files_answer_spec.rb
@@ -1,12 +1,45 @@
 require 'rails_helper'
 
 feature 'User can delete files', %q{
-  User can delete attached files from the question
+  User can delete attached files from the answer
 } do
 
-  given(:user) { create(user) }
-  given(:author) { create(author) }
-  given(:question) { create(:question, ) }
+  given(:user) { create(:user) }
+  given(:author) { create(:user) }
+  given(:question) { create(:question, user: author) }
+  given(:answer) { create(:answer, question: question, user: author) }
 
+  describe 'Authenticated user' do
+    before do
+      sign_in(author)
+      add_file_to(answer)
+      visit question_path(question)
+      fill_in 'Body', with: 'answer body'
+      attach_file 'File', ["#{Rails.root.join('spec/rails_helper.rb')}", "#{Rails.root.join('spec/rails_helper.rb').to_s}"]
+      click_on 'Answer'
+    end
 
+    scenario 'author of answer can delete the attachment' do
+      within ".attachment-#{answer.files.first.id}" do
+        click_on 'Remove attachment'
+      end
+
+      within ".answer-#{answer.id}" do
+        expect(page).to_not have_link 'rails_helper.rb'
+      end
+    end
+
+    scenario 'non author of answer try to delete the attachment' do
+      sign_in(user)
+      within ".attachment-#{answer.files.first.id}" do
+        expect(page).to_not have_link 'Remove attachment'
+      end
+    end
+
+    scenario 'guest try to delete the attachment' do
+      within ".attachment-#{answer.files.first.id}" do
+        expect(page).to_not have_link 'Remove attachment'
+      end
+    end
+  end
 end
