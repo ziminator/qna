8ad911eb37ec90c930bfe96e701f501303b11426
ziminator <aleksander.zimin@gmail.com>
2019-11-29 07:28:50 +0300

    Lesson-8 fix homework

--- spec/factories/attachments_factory.rb
+++ spec/factories/attachments_factory.rb
@@ -1,9 +1,5 @@
 FactoryBot.define do
-  #factory :body do
-  #  "answer body"
-  #end
-
-  factory :attach_file do
+  factory :attach do
     attach_file  { File.new("#{Rails.root.join('spec/rails_helper.rb')}") }
   end
 end
