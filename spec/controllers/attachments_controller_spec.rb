5e038cec9761287933ac1b04b873cc5e7531e257
ziminator <aleksander.zimin@gmail.com>
2019-12-03 22:45:02 +0300

    Lesson-8 fix homework

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -15,15 +15,18 @@ RSpec.describe AttachmentsController, type: :controller do
       it 'author can delete question attachment' do
         login(author)
         expect { delete :destroy, params: { id: question.files.first }, format: :js }.to change(question.files, :count).by(-1)
+        expect(response).to_not redirect_to question_path(question)
       end
 
       it 'user can not delete question attachment' do
         login(user)
         expect { delete :destroy, params: { id: question.files.first }, format: :js }.to_not change(question.files, :count)
+        expect(response).to redirect_to question_path(question)
       end
 
       it 'guest can not delete question attachment' do
         expect { delete :destroy, params: { id: question.files.first }, format: :js }.to_not change(question.files, :count)
+        expect(response).to redirect_to user_session_path
       end
     end
 
@@ -35,15 +38,18 @@ RSpec.describe AttachmentsController, type: :controller do
       it 'author can delete answer attachment' do
         login(author)
         expect { delete :destroy, params: { id: answer.files.first }, format: :js }.to change(answer.files, :count).by(-1)
+        expect(response).to_not redirect_to answer_path(answer.question)
       end
 
       it 'user can not delete answer attachment' do
         login(user)
         expect { delete :destroy, params: { id: answer.files.first }, format: :js }.to_not change(answer.files, :count)
+        expect(response).to redirect_to answer_path(answer.question)
       end
 
       it 'guest can not delete answer attachment' do
         expect { delete :destroy, params: { id: answer.files.first }, format: :js }.to_not change(answer.files, :count)
+        expect(response).to redirect_to user_session_path
       end
     end
   end
